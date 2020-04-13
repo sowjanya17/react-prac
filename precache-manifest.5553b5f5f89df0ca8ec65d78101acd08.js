b20d5a8ab238f88b354907c0a2a448dc8ed3fdea
Sowjanya Parimi <sowjanya.parimi@broadcom.com>
2020-04-13 16:07:41 +0530

    Updates

---
+++ precache-manifest.5553b5f5f89df0ca8ec65d78101acd08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494b436b45f4dce4ce7668c6bb5e54c0",
+    "url": "/react-prac/index.html"
+  },
+  {
+    "revision": "b0e6ed60f39447e41bd5",
+    "url": "/react-prac/static/css/main.3a2abafc.chunk.css"
+  },
+  {
+    "revision": "6e56b0e336830382e798",
+    "url": "/react-prac/static/js/2.6efbffaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-prac/static/js/2.6efbffaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e6ed60f39447e41bd5",
+    "url": "/react-prac/static/js/main.ff367c21.chunk.js"
+  },
+  {
+    "revision": "ee092aec0dd851923d26",
+    "url": "/react-prac/static/js/runtime-main.9558f4ff.js"
+  }
+]);
